fe803f2863f8c32dee4fa328b2e3104e69502ed3
Edward Shao <edwardxzshao@gmail.com>
2025-09-21 18:15:37 -0400

    eslint version fix

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// eslint.config.js
+import globals from "globals";
+import js from "@eslint/js";
+
+export default [
+  // Use the recommended rules from ESLint
+  js.configs.recommended,
+  
+  // Custom configuration object
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node // Enable Node.js global variables
+      }
+    },
+    rules: {
+      "semi": ["error", "always"] // Enforce semicolons
+    }
+  }
+];
